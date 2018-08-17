ace2c7adf514d597d9557256633f64a88d9427f9
Yana Taube <taubeyana@gmail.com>
2018-08-17 20:45:08 +0300

    started implementation of server side

---
+++ server/users/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const collection = require('./collection');
+const db = require('../db');
+
+
+
+
+router.get('/', (req,res) => {
+    db('pinterest').then(db => {
+        db.collection('users').find({}).toArray().then(data => res.send(data));
+    });
+});
+
+module.exports = router;
