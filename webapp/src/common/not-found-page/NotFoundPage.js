38a84798c75ee6bb55faf8700682bb19d739db41
Yana Taube <taubeyana@gmail.com>
2018-10-11 13:57:27 +0300

    404 page style added

--- webapp/src/common/not-found-page/NotFoundPage.js
+++ webapp/src/common/not-found-page/NotFoundPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Button from '../button/Button';
 import './NotFoundPage.css';
 import ContentWrapper from '../content-wrapper/ContentWrapper';
-
+import magnifyingGlassImg from '../../img/magnifying-glass.png'
 const NotFoundPage = () => (
     <ContentWrapper>
-        <h1> 404! Page not found </h1>
+        <div class="wrapper-404">
+            <img src={ magnifyingGlassImg }/>
+            <h1> 404 </h1>
+            <h2> OOPS! </h2>
+            <span> The page can't be found </span>
+            <Button 
+                buttonType = 'router-link' 
+                to = '/' 
+                text = 'Go Home...'
+                className = 'home-btn'>
+            </Button>
+        </div>
     </ContentWrapper>
 )
 export default NotFoundPage;
